6ce3f1d18dddd342fb851b77bb9b6ea6273b71f2
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-02-27 14:20:09 +0200

    lets gooo

--- basicProject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ basicProject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipes.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [];
 
+  constructor() {}
 }
