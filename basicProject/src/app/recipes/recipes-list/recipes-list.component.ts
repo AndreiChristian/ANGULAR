4f842d91e4f06996a56f5be862e79e838ae98322
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-02-27 14:41:57 +0200

    building the detail component

--- basicProject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ basicProject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -8,7 +7,13 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test',
+      'This is just a test',
+      'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    ),
+  ];
 
   constructor() {}
 }
