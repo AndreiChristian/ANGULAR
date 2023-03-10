d399caf6e8927d774d4f2ec26d354f8ed75d850b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-10 18:31:16 +0100

    adding routing logic

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -7,12 +7,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor( private router:Router ) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     console.log('Hello');
-    this.router.navigate(["/servers"])
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: {
+        allowEdit: 1,
+      },
+      fragment: 'loading',
+    });
   }
 }
