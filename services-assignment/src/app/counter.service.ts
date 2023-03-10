bf3ebf4fa03ab370137576c42fb47286100e1b82
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-10 14:45:04 +0100

    finished the users service

---
+++ services-assignment/src/app/counter.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CounterService {
+  counter: number = 0;
+
+  increment() {
+    this.counter++;
+    console.log(`Counter is now ${this.counter}`);
+  }
+}
