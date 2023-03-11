f32f5ba68af3030601b9b32cb2c87b6a3f7cf3a0
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-11 14:06:37 +0100

    working with forms

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') singnupForm: NgForm;
+  defaultQuestion: string = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    console.log(this.singnupForm);
   }
 }
