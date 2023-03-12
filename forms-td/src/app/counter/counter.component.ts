a24da6dcf2159e0dc61cd2db676fad30a3fe31ac
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-12 10:48:10 +0100

    experiment finished back to forms

--- forms-td/src/app/counter/counter.component.ts
+++ forms-td/src/app/counter/counter.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
+  counter: number = 0;
 
+  increment() {
+    this.counter++;
+    console.log(this.counter)
+  }
 }
