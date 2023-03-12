4b2b971400e4f3cb26597f99a3c28f6a776fe970
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-12 12:01:59 +0100

    started the assignment

--- forms-td/src/app/app.module.ts
+++ forms-td/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, AssignmentComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
