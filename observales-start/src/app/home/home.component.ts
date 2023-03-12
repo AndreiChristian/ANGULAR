ed2051632e1d85761ee2854396c60601e507eec5
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-12 15:27:46 +0100

    first contact with operators

--- observales-start/src/app/home/home.component.ts
+++ observales-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 import { count } from 'rxjs-compat/operator/count';
 
 @Component({
@@ -7,12 +7,42 @@ import { count } from 'rxjs-compat/operator/count';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+
   constructor() {}
 
   ngOnInit() {
-    interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customInterval = Observable.create((observer) => {
+      let count = 0;
+
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+        count++;
+      }, 1000);
     });
+
+    this.firstObsSubscription = customInterval.subscribe(
+      (data) => console.log(data),
+      (error) => {
+        alert(error.message);
+      },
+      () =>{
+        console.log("the oberserv")
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
   }
 }
