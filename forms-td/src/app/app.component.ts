a9b22d98683f823323d3d34377879a61e96ea0f9
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-12 11:49:49 +0100

    on to the assignment

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -9,9 +9,34 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') singnupForm: NgForm;
   defaultQuestion: string = 'pet';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
+  answer: string;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.singnupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+    this.singnupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -19,6 +44,13 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.singnupForm);
+    this.submitted = true;
+    this.user.username = this.singnupForm.value.userData.username;
+    this.user.email = this.singnupForm.value.userData.email;
+    this.user.secretQuestion = this.singnupForm.value.secret;
+    this.user.answer = this.singnupForm.value.questionAnswer;
+    this.user.gender = this.singnupForm.value.gender;
+    
+    this.singnupForm.reset();
   }
 }
