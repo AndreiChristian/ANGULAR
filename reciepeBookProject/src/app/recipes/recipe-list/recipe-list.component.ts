2c1bae64b88f30212f19e2ce03fc16f137d34714
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-21 09:14:04 +0100

    adding templates

--- reciepeBookProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ reciepeBookProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
-  recipes:[] = [];
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'test name',
+      'test description',
+      'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9'
+    ),
+    new Recipe(
+      'second test',
+      'second test description',
+      'https://www.kingarthurbaking.com/sites/default/files/styles/featured_image/public/2022-05/Tomato-Pie_0256.jpg?itok=c63mh-z9'
+    ),
+  ];
+
+  ngOnInit() {}
 }
