8c36878b99fd42a319588627acfb89818ba81eb6
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-23 21:50:05 +0100

    added custom directive

--- reciepeBookProject/src/app/app.module.ts
+++ reciepeBookProject/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownActive } from './shopping-list/shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FormsModule } from '@angular/forms';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownActive
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
