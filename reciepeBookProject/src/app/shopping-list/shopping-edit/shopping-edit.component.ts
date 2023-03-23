8b69799762e4e4862103f3d9f100304d52eea1e6
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-23 21:19:11 +0100

    hello

--- reciepeBookProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ reciepeBookProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
+  ingredientAdded = new EventEmitter<{ name: string; amount: number }>();
 }
