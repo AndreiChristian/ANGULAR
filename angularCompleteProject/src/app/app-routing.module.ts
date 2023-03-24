77b2d13acc9b1529e48c1fcf8fea3b96183440f5
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-24 17:02:53 +0100

    started working on the signup component

--- angularCompleteProject/src/app/app-routing.module.ts
+++ angularCompleteProject/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angularCompleteProject/src/app/material.module.ts
+++ angularCompleteProject/src/app/material.module.ts
@@ -3,6 +3,8 @@ import { MatCommonModule } from '@angular/material/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -10,12 +12,16 @@ import { MatIconModule } from '@angular/material/icon';
     MatCommonModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     MatSlideToggleModule,
     MatCommonModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
 })
 export class MaterialModule {}
