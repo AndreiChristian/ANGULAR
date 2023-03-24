eb22dab28d8d1f11c7c31d6285aab75034da96f1
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-24 15:23:27 +0100

    working on the services

--- reciepeBookProject/src/app/app.module.ts
+++ reciepeBookProject/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownActive } from './shopping-list/shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { DropdownActive } from './shopping-list/shared/dropdown.directive';
     DropdownActive
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- reciepeBookProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ reciepeBookProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,12 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,13 +17,13 @@ export class ShoppingEditComponent {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<{ name: string; amount: number }>();
+  constructor(private shoppingListService: ShoppingListService) {}
 
   onAddItem() {
     const newIngredient = new Ingredient(
       this.nameInputRef.nativeElement.value,
       this.amountInputRef.nativeElement.value
     );
-    this.ingredientAdded.emit(newIngredient)
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
