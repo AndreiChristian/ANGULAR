dd1631951926b1ca94c0f650245d91d5fdfd8cf5
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-01 10:58:17 +0200

    finished with input/output

--- ngrx/src/app/counter/counter-buttons/counter-buttons.component.ts
+++ ngrx/src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter-buttons',
   templateUrl: './counter-buttons.component.html',
-  styleUrls: ['./counter-buttons.component.css']
+  styleUrls: ['./counter-buttons.component.css'],
 })
 export class CounterButtonsComponent {
+  @Output() incrementEvent = new EventEmitter<void>();
+  @Output() decrementEvent = new EventEmitter<void>();
+  @Output() resetEvent = new EventEmitter<void>();
 
+  onIncrement() {
+    this.incrementEvent.emit();
+  }
+
+  onDecrement() {
+    this.decrementEvent.emit();
+  }
+
+  onReset() {
+    this.resetEvent.emit();
+  }
 }
--- ngrx/src/app/counter/counter/counter.component.ts
+++ ngrx/src/app/counter/counter/counter.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  counter: number = 0;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
+  reset() {
+    this.counter = 0;
+  }
 }
