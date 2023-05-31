3dabc51730422ba051dc10dbec0c7d79136d8ddf
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-31 17:59:02 +0200

    >

--- signals/src/app/app.module.ts
+++ signals/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    CounterOutputComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
