9ce21acdbce9ccd5db53970704e7fa58cacd00f3
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-01 17:46:52 +0200

    <

--- ngrx_i/src/app/app.module.ts
+++ ngrx_i/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { CounterComponent } from './counter/counter.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent
   ],
   imports: [
     BrowserModule,
